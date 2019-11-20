779901ebb78df4cdea941c43e5e3cc951aad7e7f
personalprojecta1 <56891804+personalprojecta1@users.noreply.github.com>
2019-11-20 09:16:03 -0500

    Create practice1.py

---
+++ practice1.py
@@ -0,0 +1,5 @@
+#Challenge:
+#Create a program that iterates and prints numbers 0 through 100, however on muliples of only 3, the program prints "Fizz". 
+#When on multiples of only 5, the program prints "Buzz". When on multiples of 3 and 5 it prints "fizzbuzz". Example:
+
+#1,2,fizz,4,buzz,fizz,7,8,fizz,buzz,11,fizz,13,14,fizzbuzz
