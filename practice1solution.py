b73ea7d2043c960f3adac01926bff1a0407156e5
personalprojecta1 <56891804+personalprojecta1@users.noreply.github.com>
2019-11-20 11:37:40 -0500

    Add files via upload

---
+++ practice1solution.py
@@ -0,0 +1,7 @@
+# For CS club - result by Julian
+for n in range(1, 101):                            # Iterate through set (1,100)
+    string = ""                                    # Declare variable to store value
+    if n % 3 == 0: string += "fizz"                # If remainder of n/3=0, add fizz to string
+    if n % 5 == 0: string += "buzz"                # If remainder of n/5=0, add buzz to string
+    if n % 3 != 0 and n % 5 != 0: string = str(n)  # If neither, add n to string
+    print(string.title())                          # Convert string to title case and print it
