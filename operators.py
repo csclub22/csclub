ff219b8544628a97e279e19157712dbd34ab2572
personalprojecta1 <56891804+personalprojecta1@users.noreply.github.com>
2019-11-20 10:33:21 -0500

    Add files via upload

---
+++ operators.py
@@ -0,0 +1,47 @@
+# operators (python) - by Julian
+# note - no bitwise
+
+# arithmetic operators
+n = 1+2   # addition
+n = 1-2   # subtraction
+n = 1*2   # multiplication
+n = 1/2   # division
+n = 1%2   # modulus
+n = 1**2  # exponentation
+n = 1//2  # floor division
+
+# assignment operators
+n = 5     # equals
+n += 5    # adds 5
+n -= 5    # subtracts 5
+n *= 5    # multiplies by 5
+n /= 5    # divides by 5
+n %= 5    # is equal to n%5
+n //= 5   # is equeal to n floor division 5
+n **= 5   # is equal to n to the power of 54
+
+# comparason operators
+if n == 11: pass                  # returns true if n = 11
+if n != 11 : pass                 # returns true if n is not 11
+if n > 11: pass                   # returns true if n is greater than 11
+if n < 11: pass                   # returns true if n is smaller than 11
+if n >= 11: pass                  # returns true if n is greater than or equal to 11
+if n <= 11: pass                  # returns true if n is smaller than or equal to 11
+
+# logical operators
+if n == 11 and n == 10+1: pass    # returns true if both left and right return true
+if n == 11 and n == 10: pass      # returns true if one of the results return true
+if not(n == 1 and n == 10): pass  # returns true if both are false
+
+# identity operators
+if n is 11: pass                  # returns true if n is 11
+if n is not 11: pass              # returns true if n is not 11
+
+#membership operators
+if n in "no": pass                # returns true if n is in "no"
+if n not in "yes": pass           # returns true if n is not in "yes"
+
+
+
+
+
